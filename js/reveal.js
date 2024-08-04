215f614531701161cc343c521d9004cfca1a227a
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-08-04 16:20:18 -0300

    biblioteca reveal adicionada

---
+++ js/reveal.js
@@ -0,0 +1,155 @@
+window.revelar = ScrollReveal({reset:true})
+
+// HEADER
+
+revelar.reveal('.efeito-container-header',
+    {
+    duration: 2000, 
+    distance: '90px',
+    origin: 'top'
+    })
+
+revelar.reveal('.efeito-container-text',
+    {
+    duration: 2000, 
+    distance: '90px'
+    })
+
+// TITULOS
+
+revelar.reveal('.titulo',
+    {
+        duration: 1000, 
+        distance: '90px',
+        origin: 'top'
+    })
+
+// BUTTONS
+
+revelar.reveal('.efeito-botao',
+    {
+        duration: 1000, 
+        distance: '90px',
+        origin: 'bottom'
+    })
+
+// PARAGRAFOS
+
+revelar.reveal('.efeito-paragrafo',
+    {
+        duration: 1000, 
+        distance: '90px',
+        origin: 'top'
+    })
+
+// SESSÃO DE BENEFICIOS
+
+revelar.reveal('.efeito-beneficios1',
+    {
+        duration: 1000, 
+        distance: '90px',
+        delay: 500
+    })
+
+revelar.reveal('.efeito-beneficios2',
+    {
+        duration: 1000, 
+        distance: '90px',
+        delay: 700
+    })
+
+revelar.reveal('.efeito-beneficios3',
+    {
+        duration: 1000, 
+        distance: '90px',
+        delay: 900
+    })
+
+// SESSÃO SOBRE NÓS
+
+revelar.reveal('.efeito-sobre-img',
+    {
+        duration: 1000, 
+        distance: '90px',
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-sobre-text',
+    {
+        duration: 1000, 
+        distance: '90px',
+        origin: 'right',
+        delay: 500,
+        scale: 0
+    })
+
+// SESSÃO DE ESTRUTURA
+
+revelar.reveal('.efeito-estrutura1',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0
+    })
+
+revelar.reveal('.efeito-estrutura2',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0
+    })
+
+revelar.reveal('.efeito-estrutura3',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0
+    })
+
+revelar.reveal('.efeito-estrutura4',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0,
+        delay: 100
+    })
+
+revelar.reveal('.efeito-estrutura5',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0,
+        delay: 100
+    })
+
+revelar.reveal('.efeito-estrutura6',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0,
+        delay: 100
+    })
+
+revelar.reveal('.efeito-estrutura7',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0,
+        delay: 150
+    })
+
+revelar.reveal('.efeito-estrutura8',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0,
+        delay: 150
+    })
+
+revelar.reveal('.efeito-estrutura9',
+    {
+        duration: 1100, 
+        distance: '90px',
+        scale: 0,
+        delay: 150
+    })
